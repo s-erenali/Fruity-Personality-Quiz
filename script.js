58232bdda473f7e15c14fa8067c4945f1a7b20dd
Glitch (personality-quiz-starter-code) <none>
2020-08-07 18:03:05 +0000

    🍃🙍 Checkpoint ./script.js:20207178/1669

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-//Global Variables
-//This is where you will define the variables you will be using in your project.
-//#TODO: Create four variables to track each possible quiz outcome
 var questionCount = 0;
 var lemonScore = 0;
 var watermelonScore = 0;
 var cherryScore = 0;
 var pineappleScore = 0;
 
+// variables for 2 pointed paired scores
 var lemoncherry = lemonScore + cherryScore;
 var lemonwatermelon = lemonScore + watermelonScore;
 var lemonpineapple = lemonScore + pineappleScore;
@@ -15,13 +13,10 @@ var pineapplewatermelon = pineappleScore + watermelonScore;
 var watermeloncherry = watermelonScore + cherryScore;
 
 
-
 var result = document.getElementById("result");
 
 
 
-
-//#TODO: Use the DOM to create variables for the first quiz question.
 
 // variables for answer choice buttons for question 1
 var q1a1 = document.getElementById("q1a1");
@@ -41,6 +36,7 @@ var q3a2 = document.getElementById("q3a2");
 var q3a3 = document.getElementById("q3a3");
 var q3a4 = document.getElementById("q3a4");
 
+// variables for answer choice buttons for question 4
 var q4a1 = document.getElementById("q4a1");
 var q4a2 = document.getElementById("q4a2");
 var q4a3 = document.getElementById("q4a3");
@@ -48,33 +44,32 @@ var q4a4 = document.getElementById("q4a4");
 
 
 
-//#TODO: Add Event Listeners to your answer choice variables.
+// event listeners for answers of question 1
 q1a1.addEventListener("click", lemon);
 q1a2.addEventListener("click", watermelon);
 q1a3.addEventListener("click", cherry);
 q1a4.addEventListener("click", pineapple);
 
+// event listeners for answers of question 2
 q2a1.addEventListener("click", pineapple);
 q2a2.addEventListener("click", lemon);
 q2a3.addEventListener("click", cherry);
 q2a4.addEventListener("click", watermelon);
 
-
+// event listeners for answers of question 3
 q3a1.addEventListener("click", lemon);
 q3a2.addEventListener("click", cherry);
 q3a3.addEventListener("click", pineapple);
 q3a4.addEventListener("click", watermelon);
 
+// event listeners for answers of question 4
 q4a1.addEventListener("click", lemon);
 q4a2.addEventListener("click", pineapple);
 q4a3.addEventListener("click", cherry);
 q4a4.addEventListener("click", watermelon);
 
-// q4a1.addEventListener("click", lemon);
 
-
-
-//#TODO: Define quiz functions here
+// functions for each fruit
 function lemon(){
   lemonScore += 1;
   questionCount += 1;
@@ -112,6 +107,7 @@ function pineapple(){
 }
 
 
+// functions for scoring system and outputs
 function updateResult() {
  
   if (lemonScore == watermelonScore && lemonwatermelon > pineapplecherry){
@@ -133,7 +129,6 @@ function updateResult() {
   }
   else if (cherryScore == pineappleScore && pineapplecherry > lemonwatermelon){
         result.innerHTML = "You are a... mix of CHERRY and PINEAPPLE!   The cherry on top~ You are an extrovert always expressing yourself and not being afraid to! People find you sweet and have an unavoidable attraction to you. But also! you are known as the wild one of the group. You are up for adventures and can be spontaneous. Basically, people think of you as a fun person! People always want to take you on trips because you will definitely spice things up.";
-
   }
   else if (lemonScore >= 3){
     result.innerHTML = "You are a... LEMON!   You can be sweet or sour, depending on who you are around. When people first meet you, it takes time for you to start being yourself around them. But once people get to know you, you become their favorite person! A refreshing addition to people's lives."; 
@@ -156,7 +151,12 @@ function updateResult() {
 
   }
   else if (cherryScore == 2){
-    
+        result.innerHTML = "You are a... CHERRY!   The cherry on top~ You are an extrovert always expressing yourself and not being afraid to! People find you sweet and have an unavoidable attraction to you.";
+
+  }
+  else if (pineappleScore == 2){
+        result.innerHTML = "You are a... PINEAPPLE!   Often known as the wild one of the group. You are up for adventures and can be spontaneous. Basically, people think of you as a fun person! People always want to take you on trips because you will definitely spice things up.";
+
   }
   else{
     result.innerHTML = "Didn't quit get that, please retake the quiz.";
@@ -167,6 +167,7 @@ function updateResult() {
 var retake = document.getElementById("restartbtn");
 retake.addEventListener("click", restart);
 
+// resetting variables after restart button
 function restart() {
   result.innerHTML = "Your personality represents the fruit... ";
   lemonScore = 0;
