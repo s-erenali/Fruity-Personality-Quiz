91ea101f988b3d5aecaf40086778d262fcca8e04
Glitch (personality-quiz-starter-code) <none>
2020-08-07 04:01:11 +0000

    📕🖼 Checkpoint ./script.js:20207178/752

--- script.js
+++ script.js
@@ -7,20 +7,25 @@ var watermelonScore = 0;
 var cherryScore = 0;
 var pineappleScore = 0;
 
+var result = document.getElementById("result");
 
 
 
 //#TODO: Use the DOM to create variables for the first quiz question.
+
+// variables for answer choice buttons for question 1
 var q1a1 = document.getElementById("q1a1");
 var q1a2 = document.getElementById("q1a2");
 var q1a3 = document.getElementById("q1a3");
 var q1a4 = document.getElementById("q1a4");
 
+// variables for answer choice buttons for question 2
 var q2a1 = document.getElementById("q2a1");
 var q2a2 = document.getElementById("q2a2");
 var q2a3 = document.getElementById("q2a3");
 var q2a4 = document.getElementById("q2a4");
 
+// variables for answer choice buttons for question 3
 var q3a1 = document.getElementById("q3a1");
 var q3a2 = document.getElementById("q3a2");
 var q3a3 = document.getElementById("q3a3");
@@ -54,12 +59,11 @@ q3a4.addEventListener("click", watermelon);
 function lemon(){
   lemonScore += 1;
   questionCount += 1;
+  alert("One Point for lemon!");
   
   if (questionCount >= 3){
-    updateResult()
-  }
-  
-  alert("One Point for lemon!");
+  updateResult()
+   }
 }
 
 function watermelon(){
@@ -86,5 +90,18 @@ function pineapple(){
   
   if (questionCount >= 3){
     updateResult()
+  }
+}
+
+
+function updateResult() {
+  if (lemonScore >= 2){
+    result.innerHTML = "LEMON"; 
+  }
+  else if (watermelonScore >= 2){
+    result.innerHTML = "WATERMELON";
+  }
+  else if (cherryScore >= 2){
+    result.innerHTML = "CHERRY";
   }
 }
